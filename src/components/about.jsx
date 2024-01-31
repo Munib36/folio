49e90d2b8237c36cedf8e89d27a1328a4e41dcb0
Munib36 <123148240+Munib36@users.noreply.github.com>
2024-01-31 16:48:49 -0500

    started on hero

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -5,6 +5,9 @@ export default function About(){
     return(
         <div className="about">
             <div className="about__hi"><h1>Hi!</h1> <img src={image} /></div>
+            <h1>I'm Munib Sondak</h1>
+            <h2>Let's keep it simple and awesome!</h2>
+            <h3>Self-taught Programmer & Junior Front-End Developer with a passion for bikes.</h3>
         </div>
     )
 }
