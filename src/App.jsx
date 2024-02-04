55385b8d9e82ad53424de18b25b2122b618a075b
Munib36 <123148240+Munib36@users.noreply.github.com>
2024-02-03 19:10:41 -0500

    did some stuff on folio

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import { useState } from 'react'
 import About from "./components/about.jsx"
 import "./react-comp-css/about.css"
 
+import Projects from "./components/projects.jsx"
+import "./react-comp-css/projects.css"
+
 function App() {
   
   return (
-    <About />
+    <>
+      <About />
+      <Projects />
+    </>
   )
 }
 
