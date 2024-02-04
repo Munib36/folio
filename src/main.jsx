fbb711040f1722f62f40f4238aaca87c528c6b57
Munib36 <123148240+Munib36@users.noreply.github.com>
2024-02-03 19:58:33 -0500

    FIXED BUG (COMPLICATED BUT I CLEANED IT LES GO)

--- src/main.jsx
+++ src/main.jsx
@@ -3,32 +3,34 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 let opacity = 1;
-let y = window.scrollY;
-let circle = document.getElementById("circle")
-let triangle = document.getElementById("triangle")
+let YScroll = window.scrollY;
+let XWidth = window.innerWidth;
+let circle = document.getElementById("circle");
+let nav = document.getElementById("nav");
+let maxPoint__scrollfunction = window.innerHeight;
 
-if(y > 400){
-    opacity = 0;
-    circle.style.opacity = opacity;
-}else{
-    opacity = 1 - (y/400);
-    circle.style.opacity = opacity;
+if (YScroll > 400) {
+	opacity = 0;
+	circle.style.opacity = opacity;
+} else {
+	opacity = 1 - (YScroll / 400);
+	circle.style.opacity = opacity;
 }
 
-window.addEventListener("scroll",()=>{
-    y = window.scrollY;
-    if(y > 400){
-        opacity = 0;
-        circle.style.opacity = opacity;
-    }else{
-        opacity = 1 - (y/400);
-        circle.style.opacity = opacity;
-        
-    }
+window.addEventListener("scroll", () => {
+	YScroll = window.scrollY;
+	if (YScroll > 400) {
+		opacity = 0;
+		circle.style.opacity = opacity;
+	} else {
+		opacity = 1 - (YScroll / 400);
+		circle.style.opacity = opacity;
+
+	}
 })
 
 function contact() {
-    window.scroll(0, window.scrollY + 100);
+	window.scroll(0, window.scrollY + 100);
 }
 
 
@@ -37,44 +39,58 @@ function contact() {
 let blur = document.querySelectorAll("#blur");
 let btn = document.getElementById("check")
 
-btn.addEventListener("change", ()=>{
-    if (btn.checked) {
-        blur.forEach(element => {
-            element.style.filter = "blur(3px)"
-        });
-      } else {
-        blur.forEach(element => {
-            element.style.filter = "blur(0px)"
-        });
-      }
+btn.addEventListener("change", () => {
+	if (btn.checked) {
+		blur.forEach(element => {
+			element.style.filter = "blur(3px)"
+		});
+	} else {
+		blur.forEach(element => {
+			element.style.filter = "blur(0px)"
+		});
+	}
 })
 
 
+function blur(){
+	blur.forEach(element => {
+		element.style.filter = "blur(3px)"
+	});
+}
+function unblur(){
+	blur.forEach(element => {
+		element.style.filter = "blur(0px)"
+	});
+}
 
 
 
 //! this is weird i know but it removes the navbar when its too low
 
-window.addEventListener("scroll",()=>{
-    y = window.scrollY;
-    if(y > window.innerHeight){ // i put window.innerHeight for responsiveness
-        document.getElementById("nav").style.display = "none"
-        blur.forEach(element => { //this blur because its kinda indirect but i cant do anything abt it
-            element.style.filter = "blur(0px)"
-        });
-    }else{
-        document.getElementById("nav").style.display = "block"
-        if(btn.checked){ // same here but it checks first if its checked
-            blur.forEach(element => { 
-                element.style.filter = "blur(3px)"
-            });
-            
-        }
-    }
+window.addEventListener("scroll", () => {
+	YScroll = window.scrollY; //* update
+
+	if (YScroll > maxPoint__scrollfunction) { 
+		nav.style.display = "none"
+	} else if ((YScroll < maxPoint__scrollfunction) && (XWidth > 650)) {
+		nav.style.display = "block"
+	} else {
+		nav.style.display = "block"
+		if (btn.checked) {
+			blur();
+		}
+	}
 })
 //! checks for change of width (just small bug but sure)
-window.addEventListener("resize", ()=>{
-  
+
+window.addEventListener("resize", () => {
+	XWidth = window.innerWidth; //* update
+
+	if (XWidth > 650 && btn.checked) {
+		unblur();
+	} else {
+		blur();
+	}
 })
 
 
@@ -83,8 +99,8 @@ window.addEventListener("resize", ()=>{
 
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render( 
+	<React.StrictMode >
+		<App />
+	</React.StrictMode>,
 )
