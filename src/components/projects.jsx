43c41ac9a5343ce89c69ab216687f9b6b564c4c8
Munib36 <123148240+Munib36@users.noreply.github.com>
2024-02-05 22:15:28 -0500

    hmm

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,7 +1,19 @@
 // import image1 from ""
 
-import { FontAwesomeIcon } from '@fontawesome/react-fontawesome';
-import { faGithub } from '@fontawesome/free-solid-svg-icons';
+function Github(){
+    return(
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/></svg>
+    )
+}
+function Link(){
+    return(
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 194.818 194.818" x="0px" y="0px" role="img" fill="var(--subtext-color)" width="194.818px" height="194.818px"><g><path d="M185.8,2.2l-56.6,0c-4.8,0-9,3.6-9.3,8.4c-0.3,5.2,3.8,9.6,9,9.6h13.5c8.1,0,12.1,9.7,6.4,15.4L78,106.2 c-3.6,3.6-3.8,9.6-0.1,13.1c1.7,1.7,4,2.5,6.2,2.5c2.3,0,4.6-0.9,6.4-2.6l70.9-70.9c5.7-5.7,15.4-1.7,15.4,6.4v13.1 c0,4.8,3.6,9,8.4,9.3c5.2,0.3,9.6-3.8,9.6-9v-57C194.8,6.2,190.8,2.2,185.8,2.2z"></path><path d="M147.7,77.3c-4.5,0.6-7.7,4.7-7.7,9.2c0,14.6,0,59.7,0,79.1c0,5-4.1,9-9,9H27c-5,0-9-4.1-9-9V61.7c0-5,4.1-9,9-9l84.4,0 c4.8,0,9-3.6,9.3-8.4c0.3-5.2-3.8-9.6-9-9.6H9c-5,0-9,4.1-9,9v139.9c0,5,4.1,9,9,9H149c5,0,9-4.1,9-9V86.2 C158,80.8,153.2,76.5,147.7,77.3z"></path></g></svg>
+    )
+}
+
+//! 20 - 25 words in descripton 
+//! 20 - 25 words in descripton 
+//! 20 - 25 words in descripton 
 
 
 function ProjectOne(){
@@ -14,15 +26,19 @@ function ProjectOne(){
             <div className="card__text">
                 <span>🏦Personal Project</span>
                 <h4>Tenzies Game</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis aliquid exercitationem deleniti sunt est tenetur architecto excepturi blanditiis cupiditate saepe incidunt rerum, eius maxime error adipisci necessitatibus, similique tempore asperiores beatae molestiae iure magnam distinctio! Dolores magnam eveniet libero doloremque error commodi sunt voluptatem! Quae quasi assumenda autem debitis modi!</p>
-                <div>
+                <p>Lorem ipsum dolo exceptur asperiores beatae gnam eveniet libero doloremque error commodi sunt voluptatem! Quae quasi assumenda autem debitis modi!</p>
+                <div className="card__text--tools">
                     <p>React</p>
                     <p>Web Game</p>
                     <p>TailwindCSS</p>
                 </div>
-                <div>
-                    <a href=""></a>
-                    <a href=""></a>
+                <div className="card__text--links">
+                    <a href="">
+                        <Github />
+                    </a>
+                    <a href="">
+                        <Link />
+                    </a>
                 </div>
             </div>
         </div>
@@ -34,15 +50,19 @@ function ProjectTwo(){
             <div className="card__text">
                 <span>🏦Personal Project</span>
                 <h4>Tenzies Game</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis aliquid exercitationem deleniti sunt est tenetur architecto excepturi blanditiis cupiditate saepe incidunt rerum, eius maxime error adipisci necessitatibus, similique tempore asperiores beatae molestiae iure magnam distinctio! Dolores magnam eveniet libero doloremque error commodi sunt voluptatem! Quae quasi assumenda autem debitis modi!</p>
-                <div>
+                <p>Lorem ipsum dolor sit ametam distinctio! Dolores magnam eveniet libero doloremque error commodi sunt voluptatem! Quae quasi assumenda autem debitis modi!</p>
+                <div className="card__text--tools">
                     <p>React</p>
                     <p>Web Game</p>
                     <p>TailwindCSS</p>
                 </div>
-                <div>
-                    <a href=""></a>
-                    <a href=""></a>
+                <div className="card__text--links">
+                    <a href="">
+                        <Github />
+                    </a>
+                    <a href="">
+                        <Link />
+                    </a>
                 </div>
             </div>
             <div>
@@ -60,15 +80,19 @@ function ProjectThree(){
             <div className="card__text">
                 <span>🏦Personal Project</span>
                 <h4>Tenzies Game</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis aliquid exercitationem deleniti sunt est tenetur architecto excepturi blanditiis cupiditate saepe incidunt rerum, eius maxime error adipisci necessitatibus, similique tempore asperiores beatae molestiae iure magnam distinctio! Dolores magnam eveniet libero doloremque error commodi sunt voluptatem! Quae quasi assumenda autem debitis modi!</p>
-                <div>
+                <p>Lorem ipsum dolor sit amet stiae iure magnam distinctio! Dolores magnam eveniet libero doloremque error commodi sunt voluptatem! Quae quasi assumenda autem debitis modi!</p>
+                <div className="card__text--tools">
                     <p>React</p>
                     <p>Web Game</p>
                     <p>TailwindCSS</p>
                 </div>
-                <div>
-                    <a href=""><FontAwesomeIcon icon={faGithub} /></a>
-                    <a href=""></a>
+                <div className="card__text--links">
+                    <a href="#">
+                        <Github />
+                    </a>
+                    <a href="#">
+                        <Link />
+                    </a>
                 </div>
             </div>
         </div>
