a4cf1b23a59b6c8a6c6caea63d029150ae73ea3c
Munib36 <123148240+Munib36@users.noreply.github.com>
2024-02-25 14:24:00 -0500

    qwe

--- src/components/who.jsx
+++ src/components/who.jsx
@@ -7,11 +7,11 @@ export default function Who(){
             <div className="who__text">
                 <h3>Who I am 🗿</h3>
                 <br />
-                <p>Hello! Im Munib Sondak, self-taught programmer and electronics hobbiest</p>
+                <p>Hello! I'm Munib Sondak, self-taught programmer and electronics hobbyist!</p>
                 <br />
-                <p>a few years ago i decided to take on programming as a career and sooner than i thought, i became who i am today</p>
+                <p>In the last few years, I departed into the rabbit hole of the world of programming and became the accustomed and skillful program cracker I am today! </p>
                 <br />
-                <p>In addition i was facinated by electricity and small electronic components, simple bits of silicon power such crazy items in our lives</p>
+                <p>The thing is, the spark of electricity and the otherworldly magic of tiny electronic components never ceases to captivate my hungry, ginormous appetite for such interesting topics. It can crack someone's noggin on how these simple bits of silicon can power the incredible gadgets we use every day.</p>
             </div>
             <div className="who__image">
                 <img src={image} alt="" />
